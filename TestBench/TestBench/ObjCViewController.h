a153707c94297e43ab5ceafaa8ce96d29c5da1da
Wiphu Blaine <Wiphu Blaine>
2020-09-20 09:52:13 +0700

    ok

---
+++ TestBench/TestBench/ObjCViewController.h
@@ -0,0 +1,18 @@
+//
+//  ObjCViewController.h
+//  TestBench
+//
+//  Created by John Blaine on 9/20/20.
+//  Copyright © 2020 John Blaine. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+@import iLogin;
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ObjCViewController : UIViewController
+@property (nonatomic, strong) iLogin *login;
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TestBench/TestBench/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  TestBench
+//
+//  Created by John Blaine on 9/20/20.
+//  Copyright © 2020 John Blaine. All rights reserved.
+//
+
+import UIKit
+import iLogin
+
+class ViewController: UIViewController {
+
+    var login : iLogin?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        login = iLogin(frame: CGRect.zero)
+        self.view.addSubview(login!)
+        
+        self.watchAction()
+    }
+
+    func watchAction() {
+        login?.watchAction(1, login: { ( usr : String?, pwd : String?) -> Bool in
+            return false
+        }, reg: { (name : String?, phone : String?) -> Bool in
+            return false
+        }, forget: { (phone : String?) -> Bool in
+            return false
+        }, createPwd: { (pwd : String?) -> Bool in
+            return false
+        }, verify: { (code : String?) -> Bool in
+            return false
+        })
+    }
+    
+    override func viewWillLayoutSubviews() {
+        let w = self.view.frame.size.width;
+        let h = self.view.frame.size.height;
+        
+        login?.frame = CGRect(x: w * 0.1, y: h * 0.1, width: w * 0.8, height: h * 0.4)
+    }
+
+}
+
