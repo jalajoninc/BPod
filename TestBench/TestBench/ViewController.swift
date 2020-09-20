d0be20bc8886337a5db93e0f7e72e94d0e54e936
Wiphu Blaine <Wiphu Blaine>
2020-09-20 16:12:32 +0700

    ok

--- TestBench/TestBench/ViewController.swift
+++ TestBench/TestBench/ViewController.swift
@@ -63,7 +63,8 @@ class ViewController: UIViewController {
         login?.frame = CGRect(x: w * 0.1, y: h * 0.1, width: w * 0.8, height: h * 0.4)
         
 //        home?.frame = CGRect(x: w * 0.1, y: h * 1.0, width: w * 0.8, height: h * 0.4)
-        rating?.frame = CGRect(x: w * 0.1, y: h * 1.0, width: w * 0.8, height: h * 0.1)
+//        rating?.frame = CGRect(x: w * 0.1, y: h * 1.0, width: w * 0.8, height: h * 0.1)
+        rating?.frame = CGRect(x: w * 0.2, y: h * 1.0, width: w * 0.6, height: h * 0.05)
     }
 
 }
