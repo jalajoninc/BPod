67d7a0b5915d68c7fa05f622b449f5a49ee2fad3
Wiphu Blaine <Wiphu Blaine>
2020-10-04 18:53:52 +0700

    ok

--- Example/Example/LogInVC.h
+++ Example/Example/LogInVC.h
@@ -1,8 +1,8 @@
 //
-//  ObjCViewController.h
+//  LogInVC.h
 //  Example
 //
-//  Created by John Blaine on 9/27/20.
+//  Created by John Blaine on 10/4/20.
 //  Copyright © 2020 John Blaine. All rights reserved.
 //
 
@@ -11,9 +11,8 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface ObjCViewController : UIViewController
+@interface LogInVC : UIViewController
 @property (nonatomic, strong) iLogin *login;
-
 @end
 
 NS_ASSUME_NONNULL_END
